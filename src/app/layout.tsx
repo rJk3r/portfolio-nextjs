fe778dcf1ae90024df45373d77c18177a29b9d76
rJk3r <redjoker@vk.com>
2025-11-07 22:46:05 +0700

    fixed fucking peace of css (COCKS SUCK SUCK) shit!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -15,7 +17,6 @@ export const metadata: Metadata = {
   title: "rJk3r",
   description: "Official rJk3r website.",
 };
-
 
 export default function RootLayout({ children }) {
   return (
