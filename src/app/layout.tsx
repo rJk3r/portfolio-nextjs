687cbc3b525e73352f09d7a350e4fcac5870a990
rJk3r <redjoker@vk.com>
2025-11-07 22:30:23 +0700

    ублюдки устранены

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Toaster } from "../components/Toaster";
 import { Geist, Geist_Mono } from "next/font/google";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,21 +16,12 @@ export const metadata: Metadata = {
   description: "Official rJk3r website.",
 };
 
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-         <body className={`${geistSans.variable} ${geistMono.variable} font-body bg-background text-foreground antialiased`}>
-        <div className="flex flex-col min-h-screen">
-          <main className="flex-grow mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
-            {children}
-          </main>
-        </div>
-        <Toaster />
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
       </body>
     </html>
   );
