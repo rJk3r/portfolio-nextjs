8a5dbbc7b5c5d44037fc6ff9681c62b265c6719a
rJk3r <93397984+rJk3r@users.noreply.github.com>
2025-11-07 16:24:24 +0700

    Revert "Add new pages to "main""

--- src/app/ideas/page.js
+++ src/app/ideas/page.js
@@ -1,21 +1,7 @@
-"use client"
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import "./UI.css";
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const token = localStorage.getItem('token');
-      if (token) {
-        router.push('/dashboard');
-      } else {
-        router.push('/login');
-      }
-    }
-  }, [router]);
-
-  return <div>rJk3r's IDEA loading...</div>;
+export default function About() {
+  return (
+    <div>
+      <h1>Ideas</h1>
+    </div>
+  );
 }
